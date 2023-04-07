25cd627a6c0bf8c02e4af3f1dc2ec366713aebab
Scott <scott@scotts-mbp.lan>
2023-04-06 17:12:25 -0700

    fix

--- components/data/getAllProducts.tsx
+++ components/data/getAllProducts.tsx
@@ -1,5 +1,5 @@
 export default async function getAllProducts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/products/`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/products`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch product");
